dd550605d2ace6db5a1e57e51c3df1e61912ae98
belen <belen.echechenique@estudiantes.utec.edu.uy>
2026-04-18 16:23:27 -0300

    agrego clase Historial y tests

--- tests/test_historial.py
+++ tests/test_historial.py
@@ -1,6 +1,5 @@
 from bioimagenes.core.historial import Historial
 
-
 def test_basico():
     h = Historial()
     assert len(h) == 0
@@ -21,10 +20,10 @@ def test_ultimo_cambio():
 
     assert h.ultimo_cambio == "C"   #verifica si el ultimo cambio es en realidad C, si no falla el test
 
-    def test_str():
-        h = Historial(["A", "B"])
+def test_str():
+    h = Historial(["A", "B"])
 
-        texto = str(h)
+    texto = str(h)
 
-        assert "Total de cambios: 2" in texto
-        assert "Último cambio: B" in texto
+    assert "Total de cambios: 2" in texto
+    assert "Último cambio: B" in texto
